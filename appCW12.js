8384f9e517f41612610047a7f67d741fb24a70a0
Nik.choporev <nik.choporev@gmail.com>
2022-11-01 14:35:39 +0200

    CW #11

---
+++ appCW12.js
@@ -0,0 +1,14 @@
+let strNum1 = "123";
+let strNum2 = "9";
+console.log(strNum1 + strNum2);
+console.log(strNum1 - strNum2);
+console.log(strNum1 > strNum2);
+
+/////////////////////////////
+
+let num1 = +strNum1;
+let num2 = + strNum2;
+console.log(strNum1 + strNum2);
+console.log(strNum1 - strNum2);
+console.log(strNum1 > strNum2);
+
