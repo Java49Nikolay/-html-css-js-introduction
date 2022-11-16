9e555bae85f4e458cfa4972cd84afa48be8fa0fe
Nik.choporev <nik.choporev@gmail.com>
2022-11-16 20:16:28 +0200

    HW #16 fix bug game is over

--- cw_14.js
+++
@@ -1,8 +0,0 @@
-function change(x){
-    x = 2 * x;
-    console.log('x in chenge:', x);
-}
-let n = 10;
-console.log("before change:", bob.neme);
-change(n);
-console.log("after change:", bob.neme);
