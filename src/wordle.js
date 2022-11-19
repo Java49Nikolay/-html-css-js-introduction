af92a0551a5e12b01aa89421abe3cc0df637a9bb
Nik.choporev <nik.choporev@gmail.com>
2022-11-19 12:29:19 +0200

    CW #17 dynamic HTML (innerHtml) for HW #17

--- src/wordle.js
+++ src/wordle.js
@@ -1,44 +1,55 @@
-const words = ["pappy", "beach", "apple", "react", "basis",
- "anger", "hello", "dress"];
+const words = ["pappy", "beach", "apple", "react", "bas",
+ "anger", "hell", "dress"];
  let word;
 const N_LETTERS = 5;
+const sectionElement = document.querySelector(".word-guess")
+sectionElement.innerHTML = getDivsElements();
 const letterElements = document.querySelectorAll(".letter-guess");
 const trialsElement = document.querySelector(".guess-trials");
 const gameOverElement = document.querySelector(".game-over-message");
 const playAgainElement = document.getElementById("play-again");
+
+let flGameOver = false;
 const INITIAL_TRIALS = 6;
 let trials = INITIAL_TRIALS;
+function getDivsElements() {
+    let index = Math.floor(Math.random() * words.length);
+   
+    let wordField = words[index];
+    let wordFieldAr = Array.from(wordField);
+    let res = wordFieldAr.map(letter => `<div class="letter-guess">${letter}</div>`);
+    return res.join('');
+}
 function showTrialsMessage(trials) {
     
-        trialsElement.innerHTML = `remained ${trials} guess trials`;
+        trialsElement.innerHTML = 
+        `remained ${trials} guess trials`;
     
    
 }
 function startGame() {
-    let index = Math.floor(Math.random() * words.length);
-    word = words[index];
-    trials = INITIAL_TRIALS
-    showTrialsMessage(trials);
-    gameOverElement.innerHTML ='';
-    playAgainElement.style.display='none';
-    letterElements.forEach(e => e.innerHTML='')
+    letterElements[2].style.background="white"
 }
 function onChange(event) {
     const wordGuess = event.target.value;
+    
+    if (flGameOver) {
+        alert("game is over");
+        return;
+    }
     trials--;
     showTrialsMessage(trials);
-    
-    event.target.value='';
+     
     if (wordGuess.length != N_LETTERS) {
         alert(`A word should contain ${N_LETTERS} letters`)
     } else {
         const wordAr = Array.from(wordGuess);
-        wordAr.forEach((l, i) => letterElements[i].innerHTML = l)
-        const colors = wordAr.map((l, i) => {
-            let index = word.indexOf(l);
+        wordAr.forEach((l, i) => letterElements[i].innerHTML = l);
+        const colors = wordAr.map((letter, i) => {
+            let index = word.indexOf(letter);
             let res = 'red';
             if (index  > -1) {
-                res = l == word[i] ? 'green' : 'yellow'
+                res = letter == word[i] ? 'green' : 'yellow'
             }
             return res;
         })
@@ -61,6 +72,7 @@ function endGame(isSuccess) {
     }
    
    playAgainElement.style.display='block';
-   trialsElement.innerHTML = ''
+   trialsElement.innerHTML = '';
+   flGameOver = true;
 }
 startGame()
