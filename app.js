01b95c76796c1520bb30c94e7601e231818e2658
Nik.choporev <nik.choporev@gmail.com>
2022-11-27 19:58:28 +0300

    HW 19 Solution

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-//const person = {id: 123, name: 'Vasya', 
-//last_name:'Ivanov', year: 2000, address: {city: 'lod', 
-//srteet: 'Sokolov', app: 100}
-///};
-//const name1 = getAddressKey();
-
-//console.log(person[name1])
-//const personArr = [123, 'Vasya', 'Ivanov', 2000, 
-//['Lod', 'Socolov', 100]]
-//console.log(personArr[3])
-//console.log(person.address);
-function displayOccurrences(strings) {
-    const occorrences = {};
-    strings.forEach(element => {
-        if (occorrences[element]) {
-            occorrences[element]= occorrences[element] + 1;
-        }else {
-            occorrences[element] = 1;
-        }
-    });
-    //console.log(Object.entries(occorrences));
-    const occorrencesAr = Object.entries(occorrences)
-    occorrencesAr.sort((e1, e2) => e2[1] - e1[1])
-    console.log(occorrencesAr);
+function createEmployee(id, name, birthYear, salary) {
+    return {id, name, birthYear, salary};
 }
-//const x = {};
-//const string = abc;
-//x[string] = 1;
-//console.log(x);"
-const strings = ["a", "lmn", "opr", "abc", 'lmn', 'abc',"lmn", "lmn", "abc", "a"];
-displayOccurrences(strings);
+const empl1 = createEmployee(123, "Vasya", 2000, 10000);
+const empl2 = empl1;
+console.log(empl1==empl2);
+function updateSalary(empl1, newSalary) {
+    empl1 = createEmployee(123, "Vasya", 2000, newSalary)
+}
+updateSalary(empl1, 15000);
+console.log(empl1);
+function updateSalaryPrimitive(salary, newSalary) {
+    salary = newSalary;
+}
+let salary = 10000;
+updateSalaryPrimitive(salary, 20000);
+console.log("salary", salary);
